9a28c2ea70788c2923727485c9bf82b7a74757fb
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-09-29 16:37:36 -0400

    added function to update json file

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 
+
+
+
+function addNote() {
+    fs.writeFile("db.json", json.stringify(notes),err => {
+        if(err)
+        throw err
+        return true
+    })
+}
+
 module.exports = app => {
     fs.readFile("db.json", "utf-8", (err, data) => {
 
@@ -8,5 +19,14 @@ module.exports = app => {
         throw err
 
         var notes = JSON.parse();
+
+        //Route time 
+        app.get("/api/notes", function(req, res) {
+            res.json(notes)
+        })
+    })
+
+    app.post("/api/notes", function(req, res) {
+
     })
 }
