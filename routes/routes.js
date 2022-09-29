709191c13cb117c9682f8c2d8dfffe9491ccaff5
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-09-29 17:20:06 -0400

    stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,44 @@
 const fs = require('fs')
 const path = require('path')
 
-
-
-
-function addNote() {
-    fs.writeFile("db.json", json.stringify(notes),err => {
-        if(err)
-        throw err
-        return true
-    })
-}
-
 module.exports = app => {
-    fs.readFile("db.json", "utf-8", (err, data) => {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
 
-        if (err) 
-        throw err
+        if (err) throw err;
 
-        var notes = JSON.parse();
+        var notes = JSON.parse(data);
 
-        //Route time 
+        function addNote() {
+            fs.writeFile("db/db.json", JSON.stringify(notes),err => {
+                if(err) throw err;
+                return true;
+            });
+        };
+
+        //View Routes
+        app.get('/notes', function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+
+        //Routes
         app.get("/api/notes", function(req, res) {
             res.json(notes)
-        })
-    })
+        });
 
-    app.post("/api/notes", function(req, res) {
+        app.post("/api/notes", function(req, res) {
+            var createNote = req.body;
+            notes.push(createNote);
+            addNote();
 
-    })
-}
+        });
+
+       
+    });
+};
+
+   
