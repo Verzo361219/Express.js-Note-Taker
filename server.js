bba95a5f7708f6d6ac413ebb9c541308a59ddd46
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-09-29 16:02:52 -0400

    finished setup on server js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const fs = require("fs")
 const express = require("express")
 const path = require("path")
-
 const app = express()
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.static(__dirname))
+
+require('./routes/routes')(app)
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: 3001")
+})
